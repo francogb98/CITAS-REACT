af36ba8e9eb034a72420ad3edf3446d96a9f4fbf
francogb98 <francobaudino98@gmail.com>
2023-02-21 10:51:48 -0300

    first and last commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+import Formulario from "./components/Formulario";
+import Header from "./components/Header";
+import ListadoPacientes from "./components/ListadoPacientes";
+
+function App() {
+  const [pacientes, setPacientes] = useState([]);
+  const [paciente, setPaciente] = useState({});
+
+  //podmeos tener varios use effect en el componente
+  //los effects se ejecutan en el orden que se van escribiendo
+  useEffect(() => {
+    const obtenerLS = () => {
+      const pacientesLS = JSON.parse(localStorage.getItem("pacientes")) ?? []; //aqui le decimos que si no hay nada en local storage agreagaremos un string vacio
+      setPacientes(pacientesLS);
+    };
+    obtenerLS();
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("pacientes", JSON.stringify(pacientes));
+  }, [pacientes]); //pacientes llega como un array lo convertimos en string
+
+  const eliminarPaciente = (id) => {
+    const pacientesActualizados = pacientes.filter(
+      (paciente) => paciente.id !== id
+    );
+    setPacientes(pacientesActualizados);
+  };
+
+  return (
+    <div className="container mx-auto mt-20">
+      <h1>Veterinaria Baudino Franco</h1>
+      <Header />
+
+      <div className="mt-12 md:flex">
+        <Formulario
+          pacientes={pacientes}
+          setPacientes={setPacientes}
+          paciente={paciente}
+          setPaciente={setPaciente}
+        />
+        <ListadoPacientes
+          pacientes={pacientes}
+          setPaciente={setPaciente}
+          eliminarPaciente={eliminarPaciente}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
